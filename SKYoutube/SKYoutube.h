6f06b5908f2d97a4e252651caf8583b74b7944a7
Shin-Kai Chen <skchen@sixnology.com>
2016-04-21 12:45:52 +0800

    Add framework project

---
+++ SKYoutube/SKYoutube.h
@@ -0,0 +1,19 @@
+//
+//  SKYoutube.h
+//  SKYoutube
+//
+//  Created by Shin-Kai Chen on 2016/4/21.
+//  Copyright © 2016年 SK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SKYoutube.
+FOUNDATION_EXPORT double SKYoutubeVersionNumber;
+
+//! Project version string for SKYoutube.
+FOUNDATION_EXPORT const unsigned char SKYoutubeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SKYoutube/PublicHeader.h>
+
+
